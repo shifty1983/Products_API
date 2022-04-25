7314f5937c90051f81796c9e62c07bfc56806ec6
Tony Greener <tony.greener@me.com>
2022-04-25 05:38:39 -0700

    Created Products Model for Products App + Registered Rest Framework and Products in Project Settings Installed Apps

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class Products(models.Model):
+   title = models.CharField(max_length=255) 
+   description = models.CharField(max_length=255)
+   price = models.DecimalField(max_digits=8, decimal_places=2)
+   inventory_quantity = models.IntegerField(default=0)
