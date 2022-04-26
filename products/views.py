e078032ec64d54e2adec733ac3c7ba229eb93a5d
Tony Greener <tony.greener@me.com>
2022-04-25 17:50:10 -0700

    Registered Car Model with Admin Site + Completed POST funcitonality for Car + Updated Rest Framework for Decimal Setting

--- products/views.py
+++ products/views.py
@@ -1,8 +1,21 @@
+import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Products
+from products import serializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
 
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
